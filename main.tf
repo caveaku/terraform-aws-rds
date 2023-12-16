50e88cb22cdeb446c0cdff85a0f782cc988dd372
unknown <caveaku>
2023-12-16 15:45:16 -0500

    update

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "random_password" "master_password" {
   count = local.create_random_password ? 1 : 0
 
   length  = var.random_password_length
-  special = false
+  special = true
 }
 
 module "db_subnet_group" {
